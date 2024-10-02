a8c9c19bcf8752d2e34f9804356335a2df7b629f
Misha Lestev <mishalestev@Mishas-MacBook-Pro.local>
2024-10-01 21:33:02 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yookassa Integration</title>
+</head>
+
+<body>
+    <form action="payment.php" method="POST">
+        <input type="number" name="price" placeholder="Amount" value="83000">
+        <input type="submit" value="Buy Subscription"></input>
+    </form>
+</body>
+
+</html>
---
+++ payment.php
@@ -0,0 +1,74 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use YooKassa\Client;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+// If form is submitted
+$PRICE = $_POST["price"];
+if (!empty($_POST)) {
+
+    // data
+    $PRICE = $_POST["price"];
+    // env
+    $LOGIN = $_ENV["SHOP_ID"];
+    $PASSWORD = $_ENV["SECRET_KEY"];
+
+    // $TEST_LOGIN = $_ENV["TEST_SHOP_ID"];
+    // $TEST_PASSWORD = $_ENV["TEST_SECRET_KEY"];
+
+    // Yookassa Initialization
+    $client = new Client();
+    $client->setAuth($LOGIN, $PASSWORD);
+
+    $payment = $client->createPayment(
+        array(
+            'amount' => array(
+                'value' => $PRICE,
+                'currency' => 'RUB',
+            ),
+            'confirmation' => array(
+                'type' => 'redirect',
+                'return_url' => 'https://www.example.com/return_url',
+            ),
+            'payment_method_data' => array(
+                'type' => 'sber_loan',
+            ),
+            'receipt' => array(
+                'items' => array(
+                    array(
+                        'description' => 'Заказ №1',
+                        'quantity' => 1,
+                        'amount' => array(
+                            'value' => $PRICE,
+                            'currency' => 'RUB',
+                        ),
+                        'vat_code' => 1, // Example VAT code, adjust as needed
+                        'payment_subject' => 'commodity', // Set appropriate payment_subject
+                        'payment_mode' => 'full_payment', // Set payment mode if necessary
+                    ),
+                ),
+                'customer' => array(
+                    'full_name' => 'Имя Фамилия', // Customer's full name
+                    'email' => 'customer@example.com', // Customer's email
+                    // or
+                    'phone' => '+79001234567', // Customer's phone number
+                ),
+            ),
+            'capture' => false,
+            'description' => 'Заказ №1',
+        ),
+        uniqid('', true)
+    );
+    
+    
+
+    // redirection
+    $confiramtion_url = $payment->getConfirmation()->getConfirmationUrl();
+    header("Location: " . $confiramtion_url);
+    exit(200);
+}
